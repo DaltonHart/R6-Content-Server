a7536fb116a6d4fe6c5ebc8f84fd9ec2ad7c61ed
Dalton Hart <DaltonVaren@gmail.com>
2019-01-17 12:37:06 -0800

    removed console log

--- controllers/verify.js
+++ controllers/verify.js
@@ -4,9 +4,7 @@ jwt = require('jsonwebtoken')
 
 module.exports = {
   verify: (req, res) => {
-    console.log(config.jwtSecret)
     let verified= jwt.verify(req.token, config.jwtSecret)
-    console.log("verified: ", verified)
     res.json(verified)
   }
 }
